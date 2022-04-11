3e62531cd72b09ebaa947e69a56a3a8ac8a44096
shauike <718401263@qq.com>
2022-04-11 14:48:48 +0800

    + 初始化

---
+++ Demo/Example/Demo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Demo_Example
+//
+//  Created by shauike on 2022/04/11.
+//  Copyright (c) 2022 shauike. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Demo/Sources/Util/Util.swift
@@ -0,0 +1,25 @@
+//
+//  Util.swift
+//  WhaleFull
+//
+//  Created by Condy on 2021/12/30.
+//
+
+import Foundation
+//import WhaleFull
+//
+//struct Util {
+//
+//    internal static let moduleName = "PROJECT"
+//}
+//
+//extension R {
+//
+//    internal static func image(_ named: String) -> UIImage {
+//        self.image(named, forResource: Util.moduleName)
+//    }
+//
+//    internal static func text(_ string: String) -> String {
+//        self.text(string, forResource: Util.moduleName)
+//    }
+//}
